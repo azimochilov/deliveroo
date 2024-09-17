d2f05669ab18a1faa6c09c341bc88f02c8088903
azimochilov <azimochilov226@gmail.com>
2024-09-17 12:37:34 +0500

    first commit

---
+++ deliveroo/src/test/java/com/deliveroo/deliveroo/DeliverooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveroo.deliveroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
